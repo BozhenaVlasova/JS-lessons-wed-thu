e10232afbe453f4904e2d42126e76501efda6e90
BozhenaVlasova <dix20sept@gmail.com>
2023-03-06 22:23:59 +0300

    done Lesson 3

--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 console.log('lesson 3');
 
 // Event loop
@@ -14,6 +16,22 @@ console.log('lesson 3');
 
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
+
+axios.get('https://jsonplaceholder.typicode.com/posts').then(res => console.log(res.data))
+
+axios.post('https://jsonplaceholder.typicode.com/posts', {
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+}).then(res => console.log(res.data))
+
+axios.put('https://jsonplaceholder.typicode.com/posts/1', {
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+}).then(res => console.log(res.data))
+
+axios.delete('https://jsonplaceholder.typicode.com/posts/1').then(res => console.log(res.data))
 
 
 // just a plug
